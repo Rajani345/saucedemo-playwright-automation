63e0cefe8ccfdb1e4c1170d27b4f56c03c1b1f29
Rajani345 <rajaniemails90@gmail.com>
2025-08-06 12:59:36 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -29,11 +29,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   //reporter: 'html',
-  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]
-  ['list'],
-  ['html', { open: 'never' }],
-  ['allure-playwright']
-],
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
