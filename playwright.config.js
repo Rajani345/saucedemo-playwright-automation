6985663b1806a0a0154f49323c83d672dd4ef182
unknown <rajaniemails90@gmail.com>
2025-08-14 14:49:16 +0530

    changes

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,7 @@ import { defineConfig, devices } from '@playwright/test';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
-  testDir: './Rajani',
+  testDir: './tests',
   timeout: 60 * 1000,
   expect: {
     timeout: 30 * 1000,
